d9d82226d7be369a16d2df347d2a715074043253
Francois Rejete <francois@rejete.com>
2012-05-14 22:36:33 +0900

    implemented memcache

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import json
-import math
-import datetime
+import pylibmc
 
 import sunazymuth
 
@@ -9,6 +8,23 @@ from flask import Flask
 from flask import request
 from flask import render_template
 app = Flask(__name__)
+
+# Check if we are on local development machine (default is prod)
+DEVELOPMENT = os.environ.get('DEVELOPMENT','FALSE')
+
+# Connect to memcache with config from environment variables.
+if DEVELOPMENT == 'TRUE':
+	# Simple non-binary connection in case of development machine
+	mc = pylibmc.Client(
+	    servers=[os.environ.get('MEMCACHE_SERVERS','127.0.0.1')]
+	)
+else:
+	mc = pylibmc.Client(
+	    servers=[os.environ.get('MEMCACHE_SERVERS','127.0.0.1')],
+	    username=os.environ.get('MEMCACHE_USERNAME',''),
+	    password=os.environ.get('MEMCACHE_PASSWORD',''),
+	    binary=True
+	)
 
 @app.route('/')
 def index():
@@ -21,9 +37,16 @@ def getEphemerides():
 
 @app.route('/findMatch', methods=['POST'])
 def findMatch():
-	lat = float(request.form['lat'])
+	global mc
+	# As request is unicode, we must transform it to a string to use as memcache key
+	lat = request.form['lat'].encode('ascii','ignore')
 	az = float(request.form['az'])
-	fullyear = sunazymuth.GetEphemerides(lat)
+	# Get the fullyear calc from the cache
+	if lat in mc:
+		fullyear = mc.get(lat)
+	else:
+		fullyear = sunazymuth.GetEphemerides(float(lat))
+		mc.set(lat, fullyear)
 	return json.dumps(sunazymuth.GetMatchingDay(fullyear,az))
 
 if __name__ == '__main__':
