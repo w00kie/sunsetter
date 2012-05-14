6c6aab61647a312fa17ab775bf8293b22186b3bc
Francois Rejete <francois@rejete.com>
2012-05-14 23:42:12 +0900

    boilerplate stuff and 404

--- app.py
+++ app.py
@@ -49,6 +49,32 @@ def findMatch():
 		mc.set(lat, fullyear)
 	return json.dumps(sunazymuth.GetMatchingDay(fullyear,az))
 
+###
+# Boiler-plate stuff from github/flask_heroku
+###
+
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+	"""Send your static text file."""
+	file_dot_text = file_name + '.txt'
+	return app.send_static_file(file_dot_text)
+
+@app.after_request
+def add_header(response):
+	"""
+	Add headers to both force latest IE rendering engine or Chrome Frame,
+	and also to cache the rendered page for 10 minutes.
+	"""
+	response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+	response.headers['Cache-Control'] = 'public, max-age=600'
+	return response
+
+@app.errorhandler(404)
+def page_not_found(error):
+	"""Custom 404 page."""
+	return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
 	#Bind to PORT if defined, otherwise default to 5000.
 	port = int(os.environ.get('PORT', 5000))
